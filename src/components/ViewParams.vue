0cfd20308c72c0344c5cd7d1162dbfcbd84fec02
AlenaMalyukova <alena.kinko@gmail.com>
2023-06-20 16:26:44 +0300

    КОмпонент для отображения отправляемых данных

---
+++ src/components/ViewParams.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <div class="content">
+      <div class="param" v-for="(value, key) in copyParams" :key="key">
+        <div class="param__name"> {{ key }}: </div>
+        <div class="param__data"> {{ value }} </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    params: Object,
+  },
+  data() {
+    return {
+      copyParams: this.params,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  width: 40%;
+  padding: 25px;
+  background: #fff;
+}
+
+.param {
+  display: flex;
+  padding-bottom: 25px;
+
+  &__name {
+    color: #888888;
+    padding-right: 15px;
+  }
+}
+</style>
