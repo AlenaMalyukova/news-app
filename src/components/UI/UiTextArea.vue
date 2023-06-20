f9684d9affde452c5da3f1b8eabde2b203a40b50
AlenaMalyukova <alena.kinko@gmail.com>
2023-06-20 16:25:10 +0300

    компонент textarea

---
+++ src/components/UI/UiTextArea.vue
@@ -0,0 +1,37 @@
+<template>
+
+  <textarea
+    class="ui-textarea"
+    type="text"
+    :value="value"
+    :placeholder="placeholder"
+    @input="$emit('update:value', $event.target.value)"
+  />
+
+</template>
+
+<script>
+  export default {
+    name: "ui-textarea",
+    props: {
+      value: String,
+      placeholder: String,
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.ui-textarea {
+  padding: 10px;
+  font-size: 18px;
+  border-radius: 5px;
+  border: 1px solid #2c3e50;
+  outline: none;
+  max-width: 400px;
+  width: 100%;
+
+  &:focus {
+    box-shadow: 0 0 15px #2c3e50;
+  }
+}
+</style>
