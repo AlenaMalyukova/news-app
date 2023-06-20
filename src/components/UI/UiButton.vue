5cad028836aa2767cb99081b0c5d2a8b2b6f1368
AlenaMalyukova <alena.kinko@gmail.com>
2023-06-20 16:24:45 +0300

    компонент кнопки

---
+++ src/components/UI/UiButton.vue
@@ -0,0 +1,37 @@
+<template>
+
+<button class="ui-button" @click="click">{{ textBtn }}</button>
+
+</template>
+
+<script>
+export default {
+  name: "ui-button",
+  props: {
+    textBtn: { type: String, default: 'Отправить' }
+  },
+  methods: {
+    click() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ui-button {
+  padding: 10px 25px;
+  width: 120px;
+  cursor: pointer;
+  background-color: #0971c7;
+  color: #fff;
+  border: none;
+  border-radius: 3px;
+  font-size: 16px;
+  font-weight: 400;
+
+  &:hover {
+    background-color: #1085e6;
+  }
+}
+</style>
