12748fe4d6ed452fc5e12773dc05499210a21f99
AlenaMalyukova <alena.kinko@gmail.com>
2023-06-20 16:24:58 +0300

    компонент input

---
+++ src/components/UI/UiInput.vue
@@ -0,0 +1,36 @@
+<template>
+
+<input
+  class="ui-input"
+  type="text"
+  :value="value"
+  :placeholder="placeholder"
+  @input="$emit('update:value', $event.target.value)"
+>
+
+</template>
+
+<script>
+export default {
+  name: "ui-input",
+  props: {
+    value: String,
+    placeholder: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.ui-input {
+  padding: 10px;
+  font-size: 18px;
+  border-radius: 5px;
+  border: 1px solid #2c3e50;
+  width: 400px;
+  outline: none;
+
+  &:focus {
+    box-shadow: 0 0 15px #2c3e50;
+  }
+}
+</style>
