0a419b43db0915aa4ffd94ba98e58d835339ed66
AlenaMalyukova <alena.kinko@gmail.com>
2023-06-20 16:25:23 +0300

    компонент checkbox

---
+++ src/components/UI/UiCheckbox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <label>
+      <input type="checkbox"
+        v-bind="inputAttrs"
+        v-model="isChecked"
+        @change="$emit('input', isChecked)"
+      >
+      <slot/>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ui-checkbox',
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      isChecked: this.value,
+    };
+  },
+  computed: {
+    inputAttrs() {
+      const { value, ...rest } = this.$attrs;
+      return rest;
+    }
+  },
+  watch: {
+    value() {
+      this.isChecked = this.value
+    },
+  }
+}
+</script>
