942ef3220e7e1981b32b3339284b58efbae11b56
AlenaMalyukova <alena.kinko@gmail.com>
2023-06-20 16:27:44 +0300

    вывод формы для добавления поста

--- src/pages/AddNews.vue
+++ src/pages/AddNews.vue
@@ -1,3 +1,45 @@
 <template>
-  <h1>Добавить новость</h1>
+  <div class="content">
+    <h1 class="contetn__title">Добавить новость</h1>
+    <div class="main">
+      <Form @send-form="sendForm"/>
+      <ViewParams v-if="params" :params="params"/>
+    </div>
+  </div>
 </template>
+
+<script>
+import Form from '@/components/Form.vue'
+import ViewParams from '@/components/ViewParams.vue'
+
+export default {
+  components: { Form, ViewParams },
+  data: () => ({
+    params: null,
+  }),
+  methods: {
+    sendForm(params) {
+      this.params = params
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  width: 72%;
+  box-sizing: border-box;
+  margin-right: 0;
+  margin-left: auto;
+  padding: 0 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.main {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
